920c88fa963fd635e13a8894739dd6bf512b8d79
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-19 16:00:36 +0100

    Revert "force sleep in thread to make test fail"
    This reverts commit 274f9d732a781eb5a525e825c2dcd4c0f860b424.

--- com.examples.school/src/main/java/com/examples/school/view/swing/StudentSwingView.java
+++ com.examples.school/src/main/java/com/examples/school/view/swing/StudentSwingView.java
@@ -137,13 +137,7 @@ public class StudentSwingView extends JFrame implements StudentView {
 		btnAdd.setEnabled(false);
 		btnAdd.addActionListener(
 			e -> new Thread(() ->
-				{
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e1) {
-					}
-					schoolController.newStudent(new Student(txtId.getText(), txtName.getText()));
-				}
+				schoolController.newStudent(new Student(txtId.getText(), txtName.getText()))
 			).start()
 		);
 		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
