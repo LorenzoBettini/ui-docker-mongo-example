2096d9453cc41dd6affdf093f061f67c7f82a373
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-19 15:55:40 +0100

    fixed ModelViewControllerIT with awaitility

--- com.examples.school/src/it/java/com/examples/school/view/swing/ModelViewControllerIT.java
+++ com.examples.school/src/it/java/com/examples/school/view/swing/ModelViewControllerIT.java
@@ -1,6 +1,9 @@
 package com.examples.school.view.swing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import java.util.concurrent.TimeUnit;
 
 import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
@@ -64,8 +67,10 @@ public class ModelViewControllerIT extends AssertJSwingJUnitTestCase {
 		window.textBox("nameTextBox").enterText("test");
 		window.button(JButtonMatcher.withText("Add")).click();
 		// ...verify that it has been added to the database
-		assertThat(studentRepository.findById("1"))
-			.isEqualTo(new Student("1", "test"));
+		await().atMost(5, TimeUnit.SECONDS).untilAsserted(() ->
+			assertThat(studentRepository.findById("1"))
+				.isEqualTo(new Student("1", "test"))
+		);
 	}
 
 	@Test
